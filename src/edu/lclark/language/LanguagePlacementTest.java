e11575e57c422094b12f20de744204a91e4a7163
Maggie Dreyer <mdreyer@lclark.edu>
2014-02-20 13:40:22 -0800

    added them for real this time

---
+++ src/edu/lclark/language/LanguagePlacementTest.java
@@ -0,0 +1,24 @@
+package edu.lclark.language;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class LanguagePlacementTest {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable()
+        {
+           public void run()
+           {
+              JFrame mainWindow = new MainWindow();
+              mainWindow.setTitle("Language Exam");
+              mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+              mainWindow.setVisible(true);
+           }
+        });
+
+	}
+
+}
---
+++ src/edu/lclark/language/MainWindow.java
@@ -0,0 +1,9 @@
+package edu.lclark.language;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame{
+	
+	
+
+}
