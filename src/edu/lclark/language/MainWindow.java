bfa32a25de9693fc8fb61b6409165d604d94e968
Connor Lay <connorlay@maclabcs8.lclark.edu>
2014-02-20 15:51:08 -0800

    Created the components for StudentTestPage and used a GridBagLayout to organize them.

--- src/edu/lclark/language/MainWindow.java
+++ src/edu/lclark/language/MainWindow.java
@@ -1,9 +1,18 @@
 package edu.lclark.language;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.*;
 
 public class MainWindow extends JFrame{
-	
-	
 
+	public MainWindow(){
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize((int)(0.75*screenSize.width), (int)(0.75 * screenSize.height));
+		
+		add(new StudentTestPage());
+		pack();
+	}
 }
--- src/edu/lclark/language/StudentTestPage.java
+++ src/edu/lclark/language/StudentTestPage.java
@@ -1,5 +1,40 @@
 package edu.lclark.language;
 
-public class StudentTestPage implements PageInteface{
+import java.awt.*;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+
+public class StudentTestPage extends JComponent implements PageInteface {
+
+	public StudentTestPage() {
+		GridBagLayout layout = new GridBagLayout();
+		setLayout(layout);
+
+		// construct components
+
+		JButton beginTestButton = new JButton("Begin Test");
+		beginTestButton.addActionListener(new BeginButtonAction());
+		JTextArea testInformation = new JTextArea("\nThis is a langauge placement exam!", 10, 20);
+		testInformation.setEditable(false);
+		ImageComponent logo = new ImageComponent("logo.jpg", 216, 154);
+		JLabel userName = new JLabel("Student");
+
+		//add components to GridBagLayout
+		add(beginTestButton, new GBC(0, 2).setWeight(0, 0).setAnchor(GBC.CENTER));
+		add(logo, new GBC(0, 1).setWeight(100, 100).setAnchor(GBC.CENTER));
+		add(testInformation, new GBC(0, 3).setWeight(100, 100).setAnchor(GBC.CENTER));
+		add(userName, new GBC(0, 0).setWeight(0, 0).setAnchor(GBC.CENTER));
+	}
+	
+	private class BeginButtonAction implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent a) {
+			System.out.println("Begin button!");
+		}
+		
+	}
 
 }
