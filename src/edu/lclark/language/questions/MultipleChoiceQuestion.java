64b6fbb74cb9c1a5cb3902029af56e6b8b6e9132
Maggie Dreyer <mdreyer@lclark.edu>
2014-03-06 15:29:54 -0800

    Added comments.

--- src/edu/lclark/language/questions/MultipleChoiceQuestion.java
+++ src/edu/lclark/language/questions/MultipleChoiceQuestion.java
@@ -1,19 +1,18 @@
 package edu.lclark.language.questions;
 
 public class MultipleChoiceQuestion implements Question {
-	
+
 	private static final int NUMBER_OF_ANSWERS = 4;
-	
+
 	private String[] answers;
 	private String question;
 	private int correctAnswerIndex;
 	private int questionID;
 	private int level;
-	
+
 	private int answerCandidateIndex;
-	
-	public MultipleChoiceQuestion()
-	{
+
+	public MultipleChoiceQuestion() {
 		answers = new String[NUMBER_OF_ANSWERS];
 	}
 
@@ -26,16 +25,16 @@ public class MultipleChoiceQuestion implements Question {
 	public int getLevel() {
 		return level;
 	}
-	
+
 	@Override
-	public void setLevel(int level){
+	public void setLevel(int level) {
 		this.level = level;
 	}
 
 	@Override
 	public void writeToFile() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- src/edu/lclark/language/questions/QuestionLoader.java
+++ src/edu/lclark/language/questions/QuestionLoader.java
@@ -5,54 +5,67 @@ import java.util.Random;
 
 import javax.xml.*;
 
-/**Loads question info from the database and constructs question objects out of it, storing them in the appropriate lists. */
+/**
+ * Loads question info from the database and constructs question objects out of
+ * it, storing them in the appropriate lists.
+ */
 public class QuestionLoader {
-	
+
 	private ArrayList<Question> questions;
-	
-	public QuestionLoader(){
+
+	public QuestionLoader() {
 		questions = new ArrayList<Question>();
 		loadQuestionsFromDatabase();
 	}
-	
-	public void loadQuestionsFromDatabase(){
-		//TODO: Read data from XML file and construct question objects, putting them into their respective lists.
+
+	/**
+	 * Reads data from the questions XML file, constructs question objects from
+	 * the data and stores them in an ArrayList.
+	 */
+	public void loadQuestionsFromDatabase() {
+		// TODO: Read data from XML file and construct question objects, putting
+		// them into their respective lists.
 		Question sampleQuestion = new MultipleChoiceQuestion();
 		sampleQuestion.setLevel(101);
 		questions.add(sampleQuestion);
 	}
-	
+
 	/**
 	 * Gets a random question of a given level.
-	 * @param level The level of the question to be returned.
+	 * 
+	 * @param level
+	 *            The level of the question to be returned.
 	 * @return A random question of the given level.
 	 */
-	public Question getQuestion(int level){
+	public Question getQuestion(int level) {
 		return getRandomQuestion(getQuestionsOfLevel(level));
 	}
-	
+
 	/**
 	 * Generates an ArrayList of questions of a specified level.
-	 * @param level The level of the questions to be filtered.
+	 * 
+	 * @param level
+	 *            The level of the questions to be filtered.
 	 * @return An ArrayList of questions of the given level.
 	 */
-	public ArrayList<Question> getQuestionsOfLevel(int level){
+	public ArrayList<Question> getQuestionsOfLevel(int level) {
 		ArrayList<Question> filteredQuestions = new ArrayList<Question>();
-		for(Question question: questions){
-			if(question.getLevel()==level){
+		for (Question question : questions) {
+			if (question.getLevel() == level) {
 				filteredQuestions.add(question);
 			}
 		}
 		return filteredQuestions;
 	}
-	
+
 	/**
 	 * Gets a random question from an ArrayList of Questions.
-	 * @param list 
+	 * 
+	 * @param list
 	 * @return A random question from the list.
 	 */
-	private Question getRandomQuestion(ArrayList<Question> list){
-		if (list.isEmpty()){
+	private Question getRandomQuestion(ArrayList<Question> list) {
+		if (list.isEmpty()) {
 			return null;
 		}
 		Random random = new Random();
