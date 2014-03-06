ad82a367ebd5c164df5b7ad8d51e41d3391baf4e
Maggie Dreyer <mdreyer@lclark.edu>
2014-03-06 13:12:45 -0800

    Added some skeleton fields to the MultipleChoiceQuestion class.

--- src/edu/lclark/language/questions/MultipleChoiceQuestion.java
+++ src/edu/lclark/language/questions/MultipleChoiceQuestion.java
@@ -1,5 +1,16 @@
 package edu.lclark.language.questions;
 
 public class MultipleChoiceQuestion extends AbstractQuestion {
-
+	
+	private static final int NUMBER_OF_ANSWERS = 4;
+	
+	private String[] answers;
+	private String question;
+	private int correctAnswerIndex;
+	private int questionID;
+	
+	public MultipleChoiceQuestion()
+	{
+		answers = new String[NUMBER_OF_ANSWERS];
+	}
 }
