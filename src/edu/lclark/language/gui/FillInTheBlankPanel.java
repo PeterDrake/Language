b53b5ef3a4dc53c8209869dc24f10c8015eb50ce
Peter Drake <drake@lclark.edu>
2014-03-11 15:38:18 -0700

    Cleaned up code in FillInTheBlankPanel.

--- src/edu/lclark/language/gui/FillInTheBlankPanel.java
+++ src/edu/lclark/language/gui/FillInTheBlankPanel.java
@@ -5,56 +5,61 @@ import java.awt.EventQueue;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 import edu.lclark.language.questions.FillInTheBlankQuestion;
 
+/** Graphic panel for displaying a FillInTheBlankQuestion. */
 public class FillInTheBlankPanel extends JPanel {
-	
+
+	/** Listens for the student to hit enter when the JTextField has focus. */
+	private class BlankListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String answer = ((JTextField) (e.getSource())).getText();
+			System.out.println(question.isCorrect(answer));
+		}
+
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	/** For testing only. */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				JFrame mainWindow = new JFrame();
+				Dimension screenSize = Toolkit.getDefaultToolkit()
+						.getScreenSize();
+				mainWindow.setSize((int) (0.75 * screenSize.width),
+						(int) (0.75 * screenSize.height));
+				mainWindow.setTitle("Fill In The Blank Test");
+				mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				mainWindow.add(new FillInTheBlankPanel(
+						new FillInTheBlankQuestion(
+								"¿Dónde _ el baño, por favor?", "está")));
+				mainWindow.setVisible(true);
+			}
+		});
+	}
+
+	/** The question associated with this panel. */
 	private FillInTheBlankQuestion question;
-	
-	public FillInTheBlankPanel(FillInTheBlankQuestion question){
+
+	public FillInTheBlankPanel(FillInTheBlankQuestion question) {
 		this.question = question;
-		for(String w : question.getWordsBeforeBlank()){
+		for (String w : question.getWordsBeforeBlank()) {
 			add(new JLabel(w));
 		}
 		JTextField blank = new JTextField(10);
 		add(blank);
 		blank.addActionListener(new BlankListener());
-		for(String w : question.getWordsAfterBlank()){
+		for (String w : question.getWordsAfterBlank()) {
 			add(new JLabel(w));
 		}
 	}
-	
-	private class BlankListener implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			String answer = ((JTextField)(e.getSource())).getText();
-			System.out.println(question.isCorrect(answer));
-		}
-		
-	}
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable()
-        {
-           public void run()
-           {
-              JFrame mainWindow = new JFrame();
-              Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-      		mainWindow.setSize((int) (0.75 * screenSize.width),
-      				(int) (0.75 * screenSize.height));
-              mainWindow.setTitle("Fill In The Blank Test");
-              mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-              mainWindow.add(new FillInTheBlankPanel(new FillInTheBlankQuestion("¿Dónde _ el baño, por favor?", "está")));
-              mainWindow.setVisible(true);
-           }
-        });
-
-	}
 }
