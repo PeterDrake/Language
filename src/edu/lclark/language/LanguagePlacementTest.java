d4735c9d044602de0fb61ed05426f1bb02089d90
Matisse Hack <olaf.hastein@gmail.com>
2014-04-22 14:04:54 -0700

    Uses HTML for student instruction page and inital file copy from jar code

--- src/edu/lclark/language/LanguagePlacementTest.java
+++ src/edu/lclark/language/LanguagePlacementTest.java
@@ -10,24 +10,38 @@ import edu.lclark.language.questions.QuestionDatabase;
 
 public class LanguagePlacementTest {
 
-	public static QuestionDatabase questionDatabase;
+    public static QuestionDatabase questionDatabase;
 
-	public static final String PATH = (System.getProperty("os.name").startsWith("Mac")) ?
-			System.getProperty("user.home") + "/Library/Application Support/Foreign Language Exam/" :
-			System.getProperty("user.dir") + File.separator;
+    public static final String PATH = (System.getProperty("os.name").startsWith("Mac")) ?
+            System.getProperty("user.home") + "/Library/Application Support/Foreign Language Exam/" :
+            System.getProperty("user.dir") + File.separator;
 
-	public static void main(String[] args) {
-		(new File(PATH)).mkdirs();
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				JFrame mainWindow = new MainWindow();
-				questionDatabase = new QuestionDatabase();
-				mainWindow.setTitle("Language Exam");
-				mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				mainWindow.setVisible(true);
-			}
-		});
+    public static void main(String[] args) {
+        File f = new File(PATH);
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+//        f = new File(PATH + "questions.xml");
+//        if (!f.isFile()) {
+////            f.createNewFile();
+//            //TODO copy file: (ClassLoader.getSystemResource("edu/lclark/language/resources/questionExamples.xml")
+//        }
+//        f = new File(PATH + "instructions.html");
+//        if (!f.isFile()) {
+////            f.createNewFile();
+//            //TODO copy file: (ClassLoader.getSystemResource("edu/lclark/language/resources/instructions.html")
+//        }
 
-	}
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame mainWindow = new MainWindow();
+                questionDatabase = new QuestionDatabase();
+                mainWindow.setTitle("Language Exam");
+                mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                mainWindow.setVisible(true);
+            }
+        });
+
+    }
 
 }
--- src/edu/lclark/language/gui/studentPage/StudentInstructionPage.java
+++ src/edu/lclark/language/gui/studentPage/StudentInstructionPage.java
@@ -11,50 +11,69 @@ import edu.lclark.language.gui.MainWindow;
 import edu.lclark.language.gui.professorPage.ProfessorPage;
 
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Scanner;
 
-/**Displays the instructions for a student about to take a test. Contains a "Begin Test" button that will send the student on to the test.*/
+/**
+ * Displays the instructions for a student about to take a test. Contains a "Begin Test" button that will send the student on to the test.
+ */
 public class StudentInstructionPage extends AbstractUserPage {
-	
-	public StudentInstructionPage(MainWindow main) {
-		super(main);
-		
-		JPanel content = new JPanel();
-		GridBagLayout layout = new GridBagLayout(); 
-		content.setLayout(layout);
-		content.setBackground(Color.WHITE);
-		title.setText("Test Instructions");
-		
-		String text = "Welcome to the test! Please read these instructions:\n\n";
-		//Reads sample test information
-		try {
-			Scanner in = new Scanner(ClassLoader.getSystemResourceAsStream("edu/lclark/language/resources/latin.txt"));
-			while (in.hasNext()){
-				text += in.nextLine() + "\n";
-			}
-		} catch (Exception e){
-			System.out.println("Error: File not found!");
-		}
-		
-		
-		// construct components
-		JButton beginTestButton = new JButton("Begin Test");
-		beginTestButton.addActionListener(new BeginButtonAction());
+
+    public StudentInstructionPage(MainWindow main) {
+        super(main);
+
+        JPanel content = new JPanel();
+        GridBagLayout layout = new GridBagLayout();
+        content.setLayout(layout);
+        content.setBackground(Color.WHITE);
+        title.setText("Test Instructions");
+
+//        String text = "Welcome to the test! Please read these instructions:\n\n";
+
+        URL htmlUrl = ClassLoader.getSystemResource("edu/lclark/language/resources/instructions.html");
+        if (htmlUrl == null) {
+            htmlUrl = ClassLoader.getSystemResource("edu/lclark/language/resources/instructions.html");
+            System.out.println("NOOOOO");
+        }
+        //Reads sample test information
+//        try {
+//            Scanner in = new Scanner(ClassLoader.getSystemResourceAsStream("edu/lclark/language/resources/latin.txt"));
+//            while (in.hasNext()) {
+//                text += in.nextLine() + "\n";
+//            }
+//        } catch (Exception e) {
+//            System.out.println("Error: File not found!");
+//        }
+
+
+        // construct components
+        JButton beginTestButton = new JButton("Begin Test");
+        beginTestButton.addActionListener(new BeginButtonAction());
 
         // Should use JTextPane for HTML formatted text
-		JTextArea testInformation = new JTextArea(text, 33, 66);
-		testInformation.setLineWrap(true);
-        testInformation.setEditable(false);
+//        JTextArea testInformation = new JTextArea(text, 33, 66);
+//        testInformation.setLineWrap(true);
+//        testInformation.setEditable(false);
 
-		ImageComponent logo = new ImageComponent("LClogo.jpg", 216, 154);
+        JTextPane testInstructions = new JTextPane();
+        try {
+            testInstructions.setPage(htmlUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ImageComponent logo = new ImageComponent("LClogo.jpg", 216, 154);
 
 //		add components to GridBagLayout
-		content.add(beginTestButton, new GBC(0, 3).setWeight(0, 0)
-				.setAnchor(GBC.CENTER));
-		content.add(logo, new GBC(0, 1).setWeight(100, 100).setAnchor(GBC.CENTER));
+        content.add(beginTestButton, new GBC(0, 3).setWeight(0, 0)
+                .setAnchor(GBC.CENTER));
+        content.add(logo, new GBC(0, 1).setWeight(100, 100).setAnchor(GBC.CENTER));
 
-        content.add(testInformation,
-				new GBC(0, 2).setWeight(100, 100).setAnchor(GBC.CENTER));
+//        content.add(testInformation,
+//                new GBC(0, 2).setWeight(100, 100).setAnchor(GBC.CENTER));
+        content.add(testInstructions,
+                new GBC(0, 2).setWeight(100, 100).setAnchor(GBC.CENTER));
 
         JScrollPane sPane = new JScrollPane(content);
         sPane.getVerticalScrollBar().setUnitIncrement(16);
@@ -66,14 +85,14 @@ public class StudentInstructionPage extends AbstractUserPage {
         //TODO Write refresh method for refresh button
     }
 
-	private class BeginButtonAction implements ActionListener {
+    private class BeginButtonAction implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent a) {
-			//System.out.println("Begin button!");
-			MainWindow main = getMainWindow();
-			main.switchPage(new ExamPage(main));
-		}
+        @Override
+        public void actionPerformed(ActionEvent a) {
+            //System.out.println("Begin button!");
+            MainWindow main = getMainWindow();
+            main.switchPage(new ExamPage(main));
+        }
 
-	}
+    }
 }
