4ef3f3d435fe39bd1d640468dbc966e7e6a0142f
Maggie Dreyer <mdreyer@lclark.edu>
2014-04-22 14:40:53 -0700

    added some features to the exam settings panel

--- src/edu/lclark/language/gui/professorPage/ExamSettingsContent.java
+++ src/edu/lclark/language/gui/professorPage/ExamSettingsContent.java
@@ -2,23 +2,25 @@ package edu.lclark.language.gui.professorPage;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridBagLayout;
 
 import javax.swing.*;
 
+import edu.lclark.language.gui.GBC;
+
 /**
- * The content panel for displaying profile information on the professor page
+ * The content panel for displaying options for various exam settings
  */
 public class ExamSettingsContent extends AbstractContent{
 	
-	private String name;
-	
-	/**
-	 * @param name the username of the professor
-	 */
 	public ExamSettingsContent(){
 		
-		JButton exportButton = new JButton("Import Database");
-		add(exportButton);
+		setLayout(new GridBagLayout());
+		
+		JLabel importExport = new JLabel("Import/Export Database");
+		add(importExport, new GBC(1,0).setAnchor(GBC.NORTH));
+		JButton exportButton = new JButton("Import");
+		add(exportButton, BorderLayout.CENTER);
 
     }
 
