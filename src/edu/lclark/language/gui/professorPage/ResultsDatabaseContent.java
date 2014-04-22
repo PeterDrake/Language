dbac61afdb65e7e169b23f089daef09278f5074e
Andrea Glaeser <aglaeser@maclabcs10.lclark.edu>
2014-04-22 15:22:20 -0700

    made results table with random info

--- src/edu/lclark/language/gui/professorPage/ResultsDatabaseContent.java
+++ src/edu/lclark/language/gui/professorPage/ResultsDatabaseContent.java
@@ -1,6 +1,11 @@
 package edu.lclark.language.gui.professorPage;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 /**
  * The content panel for viewing student results in the professor page
@@ -8,9 +13,27 @@ import javax.swing.JLabel;
 public class ResultsDatabaseContent extends AbstractContent{
 	
 	public ResultsDatabaseContent(){
+		String[] columnNames = { "Name", "Date", "Level" };
 
-		JLabel message = new JLabel("This will be a page to view student results.");
-		add(message);
+		Object[][] data = {
+				{ "Kathy", "Smith", "Snowboarding", new Integer(5),
+						new Boolean(false) },
+				{ "John", "Doe", "Rowing", new Integer(3), new Boolean(true) },
+				{ "Sue", "Black", "Knitting", new Integer(2),
+						new Boolean(false) },
+				{ "Jane", "White", "Speed reading", new Integer(20),
+						new Boolean(true) },
+				{ "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) } };
+
+		final JTable table = new JTable(data, columnNames);
+		table.setPreferredScrollableViewportSize(new Dimension(800, 600));
+		table.setFillsViewportHeight(true);
+		
+		 //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane(table);
+ 
+        //Add the scroll pane to this panel.
+        add(scrollPane);
 	}
 
 
