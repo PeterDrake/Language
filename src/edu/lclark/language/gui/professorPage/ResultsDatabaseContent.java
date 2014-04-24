fc7ad0664723b0e286af1a7e7f3c0b48e3690b3a
Andrea Glaeser <aglaeser@maclabcs10.lclark.edu>
2014-04-24 14:20:36 -0700

    completed table for student results. Added button for professor to export. Button currently does nothing.

--- src/edu/lclark/language/gui/professorPage/ResultsDatabaseContent.java
+++ src/edu/lclark/language/gui/professorPage/ResultsDatabaseContent.java
@@ -1,71 +1,88 @@
 package edu.lclark.language.gui.professorPage;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
+import edu.lclark.language.gui.GBC;
+import edu.lclark.language.gui.MainWindow;
+
+import edu.lclark.language.gui.studentPage.StudentInstructionPage;
 import edu.lclark.language.questions.QuestionInfo.QuestionLevel;
 import edu.lclark.language.studentLogic.StudentResult;
 
 /**
  * The content panel for viewing student results in the professor page
  */
-public class ResultsDatabaseContent extends AbstractContent{
-	
-	
-	public ResultsDatabaseContent(){
-		String[] columnNames = { "Name", "Date", "Level" };
-//
-//		Object[][] data = new Object[createFakeResults().size()][3]; 
-//		for (int i = 0; i < data.length; i++) {
-//			data[i][] 
-//		}
+public class ResultsDatabaseContent extends AbstractContent {
 
-		Object[][] data = {
-		        {"Kathy", "Smith",
-		         "Snowboarding", new Integer(5), new Boolean(false)},
-		        {"John", "Doe",
-		         "Rowing", new Integer(3), new Boolean(true)},
-		        {"Sue", "Black",
-		         "Knitting", new Integer(2), new Boolean(false)},
-		        {"Jane", "White",
-		         "Speed reading", new Integer(20), new Boolean(true)},
-		        {"Joe", "Brown",
-		         "Pool", new Integer(10), new Boolean(false)}
-		        };
-		
-		final JTable table = new JTable(data, columnNames);
+	public ResultsDatabaseContent() {
+		setLayout(new GridBagLayout());
+		String[] columnNames = { "Name", "Date", "Level" };
+
+		DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
+
+		final JTable table = new JTable(tableModel);
 		table.setPreferredScrollableViewportSize(new Dimension(800, 600));
 		table.setFillsViewportHeight(true);
-		
-		 //Create the scroll pane and add the table to it.
-        JScrollPane scrollPane = new JScrollPane(table);
- 
-        //Add the scroll pane to this panel.
-        add(scrollPane);
+
+		ArrayList<StudentResult> fakeResults = createFakeResults();
+		for (int i = 0; i < fakeResults.size(); i++) {
+			String name = fakeResults.get(i).getName();
+			String date = fakeResults.get(i).getDate();
+			QuestionLevel level = fakeResults.get(i).getLevel();
+
+			Object[] data = { name, date, level };
+			tableModel.addRow(data);
+		}
+
+		// Create the scroll pane and add the table to it.
+		JScrollPane scrollPane = new JScrollPane(table);
+
+		// Add the scroll pane to this panel.
+		add(scrollPane, new GBC(0, 0));
+
+		JButton exportButton = new JButton("Export");
+		exportButton.addActionListener(new ExportAction());
+		add(exportButton, new GBC(0, 1));
+
 	}
 
-	public ArrayList <StudentResult> createFakeResults(){
+	public ArrayList<StudentResult> createFakeResults() {
 		ArrayList<StudentResult> fakeResults = new ArrayList<StudentResult>();
-		
-		StudentResult s1 = new StudentResult("Andi", "1", QuestionLevel.LEVEL_101);
-		StudentResult s2 = new StudentResult("Andi1", "2", QuestionLevel.LEVEL_102);
-		StudentResult s3 = new StudentResult("Andi2", "3", QuestionLevel.LEVEL_201);
-		StudentResult s4 = new StudentResult("Andi3", "4", QuestionLevel.LEVEL_202);
-		StudentResult s5 = new StudentResult("Andi4", "5", QuestionLevel.LEVEL_301);
-		
-		fakeResults.add(s1);
-		fakeResults.add(s2);
-		fakeResults.add(s3);
-		fakeResults.add(s4);
-		fakeResults.add(s5);
-		
+
+		for (int i = 0; i < 125; i++) {
+			StudentResult s = new StudentResult("Andi" + i, "" + i,
+					QuestionLevel.LEVEL_101);
+
+			fakeResults.add(s);
+		}
+
 		return fakeResults;
-		
+
+	}
+
+	private class ExportAction implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO
+			System.out.println("helo ");
+
+		}
+
 	}
 
 }
+// tableModel idea implemented from
+// http://stackoverflow.com/questions/20526917/load-arraylist-data-into-jtable
