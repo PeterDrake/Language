e604967c70a2170b93e1753ed597709d5178f023
Maggie Dreyer <mdreyer@lclark.edu>
2014-04-29 15:41:45 -0700

    added the file this time

---
+++ src/edu/lclark/language/gui/PromptFileChooser.java
@@ -0,0 +1,32 @@
+package edu.lclark.language.gui;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import java.io.File;
+
+public class PromptFileChooser extends JFileChooser {
+
+	@Override
+	public void approveSelection() {
+		File f = getSelectedFile();
+		if (f.exists() && getDialogType() == SAVE_DIALOG) {
+			int result = JOptionPane.showConfirmDialog(this, "The file exists. Overwrite?",
+					"Existing file", JOptionPane.YES_NO_OPTION);
+			switch (result) {
+			case JOptionPane.YES_OPTION:
+				super.approveSelection();
+				return;
+			case JOptionPane.NO_OPTION:
+				return;
+			case JOptionPane.CLOSED_OPTION:
+				return;
+			case JOptionPane.CANCEL_OPTION:
+				cancelSelection();
+				return;
+			}
+		}
+		super.approveSelection();
+	}
+
+}
