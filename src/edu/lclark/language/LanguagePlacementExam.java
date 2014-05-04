2c2b86dcd42b21ec2e18cd35684383996117f1fb
Maggie Dreyer <mdreyer@lclark.edu>
2014-05-03 17:32:17 -0700

    fixed the crash when copying files from the jar to the computer

--- src/edu/lclark/language/LanguagePlacementExam.java
+++ src/edu/lclark/language/LanguagePlacementExam.java
@@ -4,8 +4,13 @@ import edu.lclark.language.gui.MainWindow;
 import edu.lclark.language.questions.QuestionDatabase;
 
 import javax.swing.*;
+
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -54,9 +59,15 @@ public class LanguagePlacementExam {
     private static void copyFileIfNull(String source, String destination){
     	 File f = new File(PATH + destination);
          try {
- 			Path sourcePath = Paths.get(ClassLoader.getSystemResource("edu/lclark/language/resources/" + source).toURI());
+ 			InputStream sourceStream = LanguagePlacementExam.class.getClass().getResourceAsStream("/edu/lclark/language/resources/" + source);
  			if (!f.isFile()) {
- 	        	Files.copy(sourcePath, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
+ 	        	BufferedReader in = new BufferedReader(new InputStreamReader(sourceStream));
+ 	        	PrintWriter out = new PrintWriter(f.toString());
+ 	        	String line;
+ 	        	while((line = in.readLine()) != null){
+ 	        		out.println(line);
+ 	        	}
+ 	        	out.close();
  	        }
  		} catch (Exception e) {
  			e.printStackTrace();
